5f4a303816d8e6e566269cddf9de9fa1d8e034d9
Tayyeba <official.tayyeba@gmail.com>
2025-04-20 19:49:54 +0100

    encoded categorical values handed

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.preprocessing import StandardScaler
 
 # Load the dataset
 data = pd.read_csv("data/dataset.csv")
@@ -20,6 +20,18 @@ data.drop(columns=['Patient_ID', 'Hospital_Name', 'Doctor_Name' ], inplace=True)
 # Encode categorical columns
 categorical_columns = data.select_dtypes(include=['object']).columns
 data = pd.get_dummies(data, columns=categorical_columns, drop_first=True)
-print(data.info())
+# print(data.info())
 
 ## DATA PREPROCESSING
+# check for missing values
+# print(data.isnull().sum())
+
+# Identify numerical columns
+numerical_columns = data.select_dtypes(include=['int64', 'float64']).columns
+print(data[numerical_columns].describe())
+# scaling numerical columns 
+columns_to_scale = ['Blood_Pressure', 'Heart_Rate', 'Treatment_Duration', 'Lab_Test_Results']
+scaler = StandardScaler()
+data[columns_to_scale] = scaler.fit_transform(data[columns_to_scale])
+print("After scaling:")
+print(data[numerical_columns].describe())
